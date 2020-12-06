a54b01732505f3e700d94ce63d3a236876dfa8c5
adamrpostjr <adamrpostjr@gmail.com>
2020-12-06 17:27:27 -0500

    Finalized Basic app and functionality

--- frontend/assets/js/client.js
+++ frontend/assets/js/client.js
@@ -1,3 +1,21 @@
+function plus(el) {
+    var special = document.getElementById(el).innerText
+    if (special != 10) {
+        var number = parseInt(special, 10) + 1
+        console.log(number);
+        document.getElementById(el).innerText = number
+    }
+}
+function minus(el) {
+    var special = document.getElementById(el).innerText
+    if (special != 1) {
+        var number = parseInt(special, 10) - 1
+        console.log(number);
+        document.getElementById(el).innerText = number
+    }
+}
+
+
 function copyElementText(id) {
     var text = document.getElementById(id).innerText;
     var elem = document.createElement("textarea");
@@ -8,3 +26,17 @@ function copyElementText(id) {
     document.body.removeChild(elem);
     alert("Copied the text: " + text);
 }
+
+async function displayPass() {
+    var password = document.getElementById('genPass')
+    var caps = document.getElementById('A-Z').checked;
+    var lower = document.getElementById('a-z').checked;
+    var number = document.getElementById('0-9').checked;
+    var special = document.getElementById('special').checked;
+    var minNumber = parseInt(document.getElementById('num').innerText, 10)
+    var minSpecial =  parseInt(document.getElementById('spec').innerText, 10)
+    
+    let generatedPassword = await mainProcess.createPassword(true,lower,number,special,minNumber,minSpecial);
+    password.innerText = generatedPassword
+}
+
--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
+const { app, BrowserWindow } = require('electron')
+const { nanoid } = require('nanoid');
 
 function createWindow () {
   const win = new BrowserWindow({
     width: 300,
-    height: 450,
+    height: 250,
+    icon: __dirname + '/frontend/assets/images/james.png',
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      enableRemoteModule: true
     }
   })
   win.removeMenu()
   win.setResizable(false)
   win.loadFile('frontend/index.html')
-  win.webContents.openDevTools()
+  // win.webContents.openDevTools()
 }
 
 app.whenReady().then(createWindow)
@@ -29,3 +32,82 @@ app.on('activate', () => {
 })
 
 
+function vowelChecker(word) {
+  var leet = ''
+  for (let i = 0; i < word.length; i++) {
+    let randNum = Math.floor(Math.random() * (100 - 0 + 1)) + 0
+    if (word.charAt(i) == 'a') {
+      randNum % 2 ? leet = leet + '@' : leet = leet + 'a'
+    } else if (word.charAt(i) == 'e') {
+      randNum % 2 ? leet = leet + 'e' : leet = leet + '3'
+    }  else if (word.charAt(i) == 'i') {
+      randNum % 5 ? leet = leet + 'i' : leet = leet + '1'
+    }  else if (word.charAt(i) == 'o') {
+      randNum % 2 ? leet = leet + '0' : leet = leet + 'o'
+    } else if (word.charAt(i) == 'u') {
+      randNum % 3 ? leet = leet + '(_)' : leet = leet + 'u'
+    } else if (word.charAt(i) == 's') {
+      randNum % 2 ? leet = leet + '$' : leet = leet + 's'
+    } else {
+      leet = leet + word.charAt(i)
+    }
+  }  
+  return leet;
+}
+function capChecker(word) {
+  let newWord = ''
+  for (let i = 0; i < word.length; i++) {
+    if (/^[a-zA-Z]*$/.test(word.charAt(i)) == true) {
+      if (word.charAt(i) != word.charAt(i).toUpperCase()) {
+        let randNum = Math.floor(Math.random() * (100 - 0 + 1)) + 0
+        if (randNum % 2) {
+          newWord = newWord + word.charAt(i).toUpperCase()
+        }else {
+          newWord = newWord + word.charAt(i)
+        }
+      }
+    } else {
+      newWord = newWord + word.charAt(i)
+    }
+  } 
+  return newWord;
+}
+
+
+
+const names = ['james', 'haywood']
+const insults = ['isdumb', 'eatsbutt', 'isacockshiner', 'isacockmuppet', 'isamouth-breather', 'isayuppie', 'isatwat', 'isaassclown', 'isaasshat', 'isaassmangler', 'isathundercunt', 'istheworstemployee', 'isacumguzzler']
+function createPassword(caps, lower, number, special, numCount, specialCount) {
+  var passSecOne = names[Math.floor(Math.random() * (names.length - 1 + 1)) + 0]
+  var salt = nanoid(3)
+  var passSecTwo = insults[Math.floor(Math.random() * (insults.length - 1 + 1)) + 0]
+  // run the first word through vowel checker
+  var leetOne = vowelChecker(passSecOne);
+  var leetSalt = salt
+  var leetTwo = vowelChecker(passSecTwo)
+  if (caps == true) {
+    var reLeetOneOnCapCheck = capChecker(leetOne)
+    if (reLeetOneOnCapCheck !== leetOne) {
+      leetOne = reLeetOneOnCapCheck
+    }
+    var reLeetTwoOnCapCheck = capChecker(leetTwo)
+    if (reLeetTwoOnCapCheck !== leetOne) {
+      leetTwo = reLeetTwoOnCapCheck
+    }
+  }else if (lower == true) {
+    // we need to run some checks
+  }else if (number == true) {
+    // we need to run some checks 
+  }else if (special == true) {
+    // we need to run some checks
+  }else if (numCount > 1) {
+    // we need to do some checks
+  }else if (specialCount > 1) {
+    // we need to do some checks
+  }
+  var leeted = leetOne+'.'+leetTwo
+  return leeted;
+}
+
+
+module.exports = { createPassword }
